b0fe4bf10962339347a8c6559329077cae806095
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-25 17:08:42 -0500

    Devise qith controleer, model and views

--- devise-facebook-oauth/config/routes.rb
+++ devise-facebook-oauth/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   devise_for :users
-=======
->>>>>>> 4b30f89915dc58d9a98af6863e3b687f609d6ff4
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+
 end
