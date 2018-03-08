e31575beb0e76e9829559f109646f6ad8c55b7ac
danielmoralesp <daniel_moralesp@hotmail.com>
2018-03-08 16:55:51 -0500

    Namespaces

--- api-codeschool/config/routes.rb
+++ api-codeschool/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :episodes # http://cs-zombies.com/episodes
 
   constraints subdomain: 'api' do
-    resources :zombies # http://api.cs-zombies.com/zombies
-    resources :humans # http://api.cs-zombies.com/humans
+    namespace :api, path: '/' do
+      resources :zombies # http://api.cs-zombies.com/zombies
+      resources :humans # http://api.cs-zombies.com/humans
+    end
   end
+
+  resources :episodes # http://cs-zombies.com/episodes
 end
