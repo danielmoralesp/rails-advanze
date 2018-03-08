372f4c72f5036e4817fac031aa0b2aca77060855
danielmoralesp <daniel_moralesp@hotmail.com>
2018-03-08 16:37:59 -0500

    with_options routes

--- api-codeschool/config/routes.rb
+++ api-codeschool/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  with_options only: [:index, :edit, :update] do |list_only|
+    list_only.resources :zombies
+    list_only.resources :humans
+    list_only.resources :medical_kits
+  end
 end
