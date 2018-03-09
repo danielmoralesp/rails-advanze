cd7bb2d9b131165ec59211b1ec0eaa97d10f4942
danielmoralesp <daniel_moralesp@hotmail.com>
2018-03-09 15:38:42 -0500

    add post endpoint

---
+++ ReadingList/app/controllers/finished_books_controller.rb
@@ -0,0 +1,24 @@
+class FinishedBooksController < ApplicationController
+  def index
+    books = Book.finished
+    respond_to do |format|
+      format.json { render json: books, status: 200 }
+      format.xml { render xml: books, status: 200 }
+    end
+  end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: 201, location: book
+    else
+      render json: book.errors, status: 422
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :rating)
+    end
+end
