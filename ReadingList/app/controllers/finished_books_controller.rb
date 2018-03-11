f2c1d8a7c4208f69d8f583e7f320fa739b292ff8
danielmoralesp <daniel_moralesp@hotmail.com>
2018-03-11 15:52:30 -0500

    Add active model serializers and other data for models

--- ReadingList/app/controllers/finished_books_controller.rb
+++ ReadingList/app/controllers/finished_books_controller.rb
@@ -6,25 +6,4 @@ class FinishedBooksController < ApplicationController
       format.xml { render xml: books, status: 200 }
     end
   end
-
-  def create
-    book = Book.new(book_params)
-
-    if book.save
-      render json: book, status: 201, location: book
-    else
-      render json: book.errors, status: 422
-    end
-  end
-
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy!
-    render nothing: true, status: 204
-  end
-
-  private
-    def book_params
-      params.require(:book).permit(:title, :rating)
-    end
 end
