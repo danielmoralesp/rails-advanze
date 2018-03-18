b823d979035e00ae95675f6d8d53f3450472a372
danielmoralesp <daniel_moralesp@hotmail.com>
2018-03-17 19:25:56 -0500

    Model, migrations and seed

--- ideaboard-api/db/seeds.rb
+++ ideaboard-api/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Idea.delete_all
+
+ideas = Idea.create!(
+  [
+    {
+      title: "A new cake recipe",
+      body: "made of chocolate"
+    },
+    {
+      title: "A twitter client idea",
+      body: "Onl for repliyin to mentions and DMs"
+    },
+    {
+      title: "A novel set in Italy",
+      body: "A mafia crime data"
+    },
+    {
+      title: "card game design",
+      body: "like uno but involves drinking"
+    }
+  ]
+)
